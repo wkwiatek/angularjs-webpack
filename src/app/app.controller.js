e23b2a8351f2406a0ae92c63c0c459003da1cb83
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2016-05-10 00:35:53 +0200

    Dodanie serwisu [Service]

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -1,10 +1,5 @@
-export function AppController() {
+export function AppController(AppService) {
 	const vm = this
-
-	//5/ Dodajmy listę produktów do kupienia
-	vm.products = [
-		{ name: 'Milk', price: 1.05 },
-		{ name: 'Salami', price: 2.5 },
-		{ name: 'Bread', price: 0.85 },
-	]
+	
+	vm.products = AppService.products 
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import angular from 'angular'
 import { AppController } from './app/app.controller'
+import { AppService } from './app/app.service'
 
 angular
 	.module('app', [])
-	.controller('AppController', AppController)
+	/// Dzięki strict DI dostaniemy błąd bez jawnego podania wstrzykiwanych zależności
+	.controller('AppController', ['AppService', AppController])
+	.service('AppService', AppService)
