14dd435bc0cd40318ac889aba16c3cf8938e939b
Andrzej Kopeć <kapkowski@gmail.com>
2016-10-25 21:11:45 +0200

    Move script reference to head with defer attribute

--- 1-2-angular-products/004/src/index.js
+++ 1-2-angular-products/004/src/index.js
@@ -1,9 +1,8 @@
-import angular from 'angular'
-import { AppController } from './app/app.controller'
-import { AppService } from './app/app.service'
+import angular from 'angular';
+import { AppController } from './app/app.controller';
+import { AppService } from './app/app.service';
 
 angular
-	.module('app', [])
-	/// Thanks to strict di mode we'll get error without explicit dependency declaration
-	.controller('AppController', ['AppService', AppController])
-	.service('AppService', AppService)
+    .module('app', [])
+    .controller('AppController', AppController)
+    .service('AppService', AppService);
