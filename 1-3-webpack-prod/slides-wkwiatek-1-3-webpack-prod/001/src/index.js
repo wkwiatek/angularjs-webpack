793288f38bd6bdac3c82775b0dc1fb915910e9bd
Andrzej Kopeć <kapkowski@gmail.com>
2016-10-25 22:17:34 +0200

    Fix problems with ENV variables by using --env param in webpack

--- 1-3-webpack-prod/slides-wkwiatek-1-3-webpack-prod/001/src/index.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular'
-import { AppController } from './app/app.controller'
-import { AppService } from './app/app.service'
-
-angular
-	.module('app', [])
-	.controller('AppController', ['AppService', AppController])
-	.service('AppService', AppService)
