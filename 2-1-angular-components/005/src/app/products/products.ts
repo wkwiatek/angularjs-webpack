61d7085b49e6db25e727fff8fb8c3aca2ac353b6
Andrzej Kopeć <kapkowski@gmail.com>
2016-10-29 19:06:37 +0200

    Add component output slides

--- 2-1-angular-components/005/src/app/products/products.ts
+++ 2-1-angular-components/005/src/app/products/products.ts
@@ -1,12 +1,10 @@
 import IModule = angular.IModule
 import { ProductsComponent } from './products.component'
 import { ProductsService } from './products.service'
-/// Importing new component
 import { ProductsAddComponent } from './add/products-add.component'
 
 export const ProductsModule: IModule = angular
   .module('app.components.products', [])
   .component('products', ProductsComponent)
-  /// And registering it
   .component('productsAdd', ProductsAddComponent)
   .service('Products', ProductsService)
